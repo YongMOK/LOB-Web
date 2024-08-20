e81fd2b940ebc264777878384336724244e52f5d
RayongMOK <mokyong20801@gmail.com>
2024-08-20 14:43:05 +0200

    no message

--- frontend/train_model.py
+++ frontend/train_model.py
@@ -1,2 +1,5 @@
 import streamlit as st
-st.write("select market")
+
+st.title("MAP433")
+st.markdown("""MAP433 est un cours mathematique appliqué à l'ecole polytechnique. Il est un cours statistique qui se compose de 
+         deux partis. Le premier est la partie statistique et la deuxieme est la partie apprendisage marc""")
