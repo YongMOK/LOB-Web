0bdc7e3b6b459e7c9387939a467b04689c30888c
YongMOK <120304876+YongMOK@users.noreply.github.com>
2024-09-13 13:00:26 +0200

    Delete frontend directory

--- frontend/train_model.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-st.title("MAP433")
-st.markdown("""MAP433 est un cours mathematique appliqué à l'ecole polytechnique. Il est un cours statistique qui se compose de 
-         deux partis. Le premier est la partie statistique et la deuxieme est la partie apprendisage marc""")
